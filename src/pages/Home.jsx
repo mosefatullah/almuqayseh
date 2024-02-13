2e622c740461806d52bb705b867b98dd0268a82f
saadalamin <saadalamin2004@gmail.com>
2024-02-13 19:37:22 +0600

    Design Improved + New Navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 
 // Components
 import Navbar from "./components/Navbar";
@@ -9,6 +10,12 @@ import Footer from "./components/Footer";
 import HeroBanner from "/images/interface/herobanner.jpg";
 import IntroBanner1 from "./../assets/civil-engineer-construction-worker.jpg";
 import IntroBanner2 from "./../assets/crop-architect-opening-blueprint-scaled.jpg";
+import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
+import DomainIcon from "@mui/icons-material/Domain";
+import ConstructionIcon from "@mui/icons-material/Construction";
+import TagIcon from "@mui/icons-material/Tag";
+import EngineeringIcon from "@mui/icons-material/Engineering";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 function Home() {
   return (
@@ -29,9 +36,7 @@ function Home() {
             <div className="container">
               <div className="row">
                 <div className="col-12 col-lg-8 d-flex flex-column h-100vh justify-content-center">
-                  <div className="hero-chip">
-                    Connects you with the best workers
-                  </div>
+                  <div className="hero-chip">Government Approved C.R. 1010630351</div>
                   <h1 className="hero-heading">
                     We are Al-Muqayseh Gen Cont Est.
                   </h1>
@@ -41,7 +46,7 @@ function Home() {
                     from. We are dedicated.
                   </p>
                   <button className="hero-button btn btn-primary">
-                    Get Started
+                    <Link to="/contact">Consult Now</Link>
                   </button>
                 </div>
               </div>
@@ -55,6 +60,13 @@ function Home() {
               <div className="col-12 col-lg-6">
                 <h2 className="intro-heading">
                   We turn your ideas into reality
+                  <TipsAndUpdatesIcon
+                    sx={{
+                      fontSize: 40,
+                      marginLeft: "10px",
+                      opacity: "0.2",
+                    }}
+                  />
                 </h2>
                 <hr className="line-bar" />
                 <p className="intro-paragraph">
@@ -66,7 +78,7 @@ function Home() {
                   operation.
                 </p>
                 <button className="intro-button btn btn-primary">
-                  Read More
+                  <Link to="/about">Read More</Link>
                 </button>
               </div>
               <div className="col-12 col-lg-6">
@@ -85,7 +97,7 @@ function Home() {
         {/*-- __SERVICES__ --*/}
         <div className="services-section">
           <div className="container">
-            <div className="row">
+            <div className="row row-gap-4">
               <div className="col-12">
                 <p className="services-chip">We Are Innovative</p>
                 <h2 className="services-heading">Our Services</h2>
@@ -93,43 +105,63 @@ function Home() {
               </div>
               <div className="col-12 col-lg-4">
                 <div className="service-card">
+                  <DomainIcon />
                   <h3>Mechanical Works</h3>
                   <p>
-                    We are dedicated to helping you find the best workers for
-                    your business. We have a wide range of workers to choose
-                    from. We are dedicated.
+                    All piping works necessary to provide a complete, functional
+                    facility in accordance with the project requirements,
+                    applicable codes and standards, and project specifications
+                    to the satisfaction of Clients.
                   </p>
                 </div>
               </div>
               <div className="col-12 col-lg-4">
                 <div className="service-card">
+                  <ConstructionIcon />
                   <h3>Construction</h3>
                   <p>
-                    We are dedicated to helping you find the best workers for
-                    your business. We have a wide range of workers to choose
-                    from. We are dedicated.
+                    High demand for construction manpower in Saudi Arabia due to
+                    rapid economic growth and infrastructure development. We
+                    specialize in supplying skilled and semi-skilled
+                    professionals.
                   </p>
                 </div>
               </div>
               <div className="col-12 col-lg-4">
                 <div className="service-card">
+                  <TagIcon />
                   <h3>Manpower Supply</h3>
                   <p>
-                    We are dedicated to helping you find the best workers for
-                    your business. We have a wide range of workers to choose
-                    from. We are dedicated.
+                    We prefer oral or convenient testing for candidate
+                    selection, coordinated by the employer or their assign. We
+                    handle settlement, transportation, and other necessary
+                    support.
                   </p>
                 </div>
               </div>
               <div className="col-12 col-lg-4">
                 <div className="service-card">
+                  <EngineeringIcon />
                   <h3>Civil Works</h3>
                   <p>
-                    We are dedicated to helping you find the best workers for
-                    your business. We have a wide range of workers to choose
-                    from. We are dedicated.
+                    Well versed in engineering and construction expertise
+                    includes traditional disciplines, Civil, Mechanical,
+                    Electrical, structural engineering as well as process and
+                    mechanical design.
                   </p>
                 </div>
+              </div>
+              <div className="col-12 text-center">
+                <button className="services-button btn text-white mt-3">
+                  <Link to="/services">
+                    View all services
+                    <KeyboardArrowRightIcon
+                      sx={{
+                        mt: "-3px",
+                      }}
+                    />
+                  </Link>
+                </button>
               </div>
             </div>
           </div>
--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -3,19 +3,34 @@ import { NavLink } from "react-router-dom";
 
 // Assets
 import Logo from "../../assets/Al-Muqayseh.png";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 
 function Navbar() {
- return (
-  <>
-   <nav className="navbar">
-    <div className="container">
-     <NavLink className="navbar-brand" to="/">
-      <img src={Logo} alt="Al Muqayseh Contracting Est" />
-     </NavLink>
-    </div>
-   </nav>
-  </>
- );
+  return (
+    <>
+      <nav className="navbar">
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">
+            <img src={Logo} alt="Al Muqayseh Contracting Est" />
+            <div className="logo">
+              <h3>Al-Muqayseh</h3>
+              <p>General Contracting Est.</p>
+            </div>
+          </NavLink>
+          <div className="navbar-items">
+            <p className="contact-info">
+              CALL US! <span>(+966) 501163626</span>
+            </p>
+            <div className="menu-action">
+              <button className="btn btn-primary">
+                <MenuRoundedIcon />
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
